9da0aa73803993951cfe19942de1b3e1f28dd84d
Nazar Niphone <74293442+nazar1ua@users.noreply.github.com>
2022-10-03 09:58:31 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -26,7 +26,7 @@ git commit -m "Розгортання $(date +"%T %D")"
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:nazar1ua/vue-schedule.git gh-pages:gh-pages
+git push -f git@github.com:newlancers/greenrute.git gh-pages:gh-pages
 
 cd ..
 rm -rf dist

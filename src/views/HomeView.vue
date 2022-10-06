7b16318e4cec21f4eaaf2f9215ad5c97259d1540
Nazar Niphone <ndzobas@gmail.com>
2022-10-06 22:44:41 +0300

    Виправлено помилку

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,17 @@
 <script lang="ts" setup>
-import type {Lessons} from '@/helpers/lessons'
+import type {Lessons, Lesson} from '@/helpers/lessons'
 import type {TimeResponse} from '@/helpers/date'
 import Table from '@/components/Table.vue'
 
 interface Props {
   lessons: Lessons
   time: TimeResponse
+  lesson: Lesson
 }
 
 defineProps<Props>()
 </script>
 
 <template>
-  <Table :lessons="lessons" :time="time" />
+  <Table :lessons="lessons" :time="time" :lesson="lesson" />
 </template>

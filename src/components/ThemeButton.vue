3db316a4f8c0e71122cb2b76fc2a2c27ec2e7499
Nazar Niphone <74293442+nazar1ua@users.noreply.github.com>
2022-10-25 21:14:25 +0300

    Update ThemeButton.vue

--- src/components/ThemeButton.vue
+++ src/components/ThemeButton.vue
@@ -23,6 +23,6 @@ const dark = computed(() => (
   <button @click="changeTheme(false, store)" class="inline-flex items-center justify-center rounded-md p-2 text-zinc-400 hover:bg-zinc-100 dark:hover:bg-zinc-700 hover:text-zinc-500 dark:hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500 dark:focus:ring-white">
     <span class="sr-only">Змінити тему</span>
     <SunIcon v-if="store.theme === 'dark' || dark" aria-hidden="true" class="block h-6 w-6"/>
-    <MoonIcon v-else-if="store.theme === 'dark' || !dark" aria-hidden="true" class="block h-6 w-6"/>
+    <MoonIcon v-else aria-hidden="true" class="block h-6 w-6"/>
   </button>
 </template>

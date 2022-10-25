256cf362a37db31e73d1cafaa30fb5c80c75a970
Nazar Niphone <74293442+nazar1ua@users.noreply.github.com>
2022-10-25 21:10:56 +0300

    Update ThemeButton.vue

--- src/components/ThemeButton.vue
+++ src/components/ThemeButton.vue
@@ -1,7 +1,8 @@
 <script lang="ts" setup>
 import {useThemeStore} from '@/stores/theme'
-import {DevicePhoneMobileIcon, MoonIcon, SunIcon} from '@heroicons/vue/24/outline'
+import {MoonIcon, SunIcon} from '@heroicons/vue/24/outline'
 import {changeTheme} from '@/helpers/theme'
+import {computed} from 'vue'
 
 interface Props {
   mode?: 'small' | 'expanded'
@@ -12,13 +13,16 @@ withDefaults(defineProps<Props>(), {
 })
 
 const store = useThemeStore()
+
+const dark = computed(() => (
+  window.matchMedia('(prefers-color-scheme: dark)').matches
+))
 </script>
 
 <template>
   <button @click="changeTheme(false, store)" class="inline-flex items-center justify-center rounded-md p-2 text-zinc-400 hover:bg-zinc-100 dark:hover:bg-zinc-700 hover:text-zinc-500 dark:hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500 dark:focus:ring-white">
     <span class="sr-only">Змінити тему</span>
-    <SunIcon v-if="store.theme === 'light'" aria-hidden="true" class="block h-6 w-6"/>
-    <MoonIcon v-else-if="store.theme === 'dark'" aria-hidden="true" class="block h-6 w-6"/>
-    <DevicePhoneMobileIcon v-else aria-hidden="true" class="block h-6 w-6"/>
+    <SunIcon v-if="store.theme === 'dark' || dark" aria-hidden="true" class="block h-6 w-6"/>
+    <MoonIcon v-else-if="store.theme === 'dark' || !dark" aria-hidden="true" class="block h-6 w-6"/>
   </button>
 </template>
